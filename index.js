c6716c60c75713ac9e946e32a654dff41cf94ca7
soepaing <sopykt09@gmail.com>
2019-01-04 20:33:01 +0630

    add access token to test

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 const fbUpload = require('facebook-api-video-upload');
  
 const args = {
-    token: y, // with the permission to upload
-    id: me, //The id represent {page_id || user_id || event_id || group_id}
-    stream: fs.createReadStream('./test/fixture/fixture.mp4') //path to the video,
+    token: 
+EAAZAMUkYKVxYBAFHWDXW817S1FBZBDqMNaDVOFrhF63Xm0yxZBRZBHPOOTZBM9B8n3hGRZAPi2VE6ZArz5DgKEPoD54WUXXsMopWvRPKZBcM5W1tkjlI9fCu7yK5SqpT3ZAx39Xsmpz8pk169teFHvImbfXn9dGBnZA77WLvlNokLmbgs6lqnQ3ntxFpKY9chRZBZCkRq447UMAXpQZDZD, 
+// with the permission to upload
+    id: "me", //The id represent {page_id || user_id || event_id || group_id}
+    stream: fs.createReadStream('received_218257442429267.mp4') //path to the 
+video,
     title: "my video",
     description: "my description"
 };
